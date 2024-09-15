09c05b8625b68ac342ecf278d9ce50d7e1c39bae
srija-pavishetti <119001557+srija-pavishetti@users.noreply.github.com>
2024-09-15 08:20:24 +0530

    Update brandsRouter.js

--- routes/brandsRouter.js
+++ routes/brandsRouter.js
@@ -10,7 +10,7 @@ import isAdmin from "../middlewares/isAdmin.js";
 
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
-const brandsRouter = exppress.Router();
+const brandsRouter = express.Router();
 
 brandsRouter.post("/", isLoggedIn, isAdmin, createBrandCtrl);
 brandsRouter.get("/", getAllBrandsCtrl);
